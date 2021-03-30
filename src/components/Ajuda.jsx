c188ee464fed15e43de49245b3473701ab912d8f
joaomonteiroSN <70484799+joaomonteiroSN@users.noreply.github.com>
2021-03-30 14:16:11 -0300

    um pouco mais de collapsible

--- src/components/Ajuda.jsx
+++ src/components/Ajuda.jsx
@@ -4,22 +4,43 @@ import Collapsible from 'react-collapsible';
 function Ajuda() {
     return (
         <div className={styles.ajuda} >
-            <Collapsible trigger="Start here">
+            <h1>Algumas das principais dúvidas:</h1>
+            <Collapsible trigger="Como começar">
                 <p>
-                    This is the collapsible content. It can be any element or React
-                    component you like.
+                    Você pode baixar o Spotify grátis para <a href="https://www.spotify.com/br/download/windows/">desktop aqui </a>
+                    ou acessar uma loja de aplicativos móveis para baixar
+                    o aplicativo do Spotify no seu celular ou tablet.
+                </p>
+                <p>
+                    Se não quiser baixar o aplicativo, outra opção é acessar
+                    o nosso web player para começar a ouvir direto no seu navegador.
                 </p>
             </Collapsible>
-            <Collapsible trigger="Mais um">
+
+            <Collapsible trigger="Que tipo de assinaturas o Spotify oferece">
                 <p>
-                    This is the collapsible content. It can be any element or React
-                    component you like.
+                    Você pode baixar e usar o Spotify gratuitamente
+                    ou escolher entre um dos nossos planos Premium.
                 </p>
             </Collapsible>
-            <Collapsible trigger="Mais dois">
+
+            <Collapsible trigger="Cancelamento">
                 <p>
-                    This is the collapsible content. It can be any element or React
-                    component you like.
+                    Cancele seu plano Premium a qualquer momento na página da
+                    sua conta.
+                </p>
+                <ol>
+                    <li>Entre em spotify.com/account.</li>
+                    <li>Em Seu plano, clique em MUDAR DE PLANO.</li>
+                    <li>Role para baixo até Spotify Free e clique em CANCELAR O PREMIUM.</li>
+                </ol>
+                <p>
+                    Sua conta continuará no Premium até a próxima data de cobrança.
+                    Depois disso, ela passará para a versão gratuita.
+                </p>
+                <p>
+                    Você não vai perder suas playlists e músicas salvas quando passar para o Free.
+                    Você ainda vai poder entrar e ouvir seu som, mas com anúncios.
                 </p>
             </Collapsible>
         </div>
