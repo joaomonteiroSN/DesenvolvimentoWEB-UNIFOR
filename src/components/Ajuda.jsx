0705b65ebd7e4af1e1c62b5082839f3392c1665a
joaomonteiroSN <70484799+joaomonteiroSN@users.noreply.github.com>
2021-03-30 13:15:50 -0300

    adicionando collapsible react

--- src/components/Ajuda.jsx
+++ src/components/Ajuda.jsx
@@ -1,11 +1,31 @@
 import styles from '../styles/components/Ajuda.module.css';
+import Collapsible from 'react-collapsible';
 
 function Ajuda() {
     return (
         <div className={styles.ajuda} >
-            <h1>HELLLLP</h1>
+            <Collapsible trigger="Start here">
+                <p>
+                    This is the collapsible content. It can be any element or React
+                    component you like.
+                </p>
+            </Collapsible>
+            <Collapsible trigger="Mais um">
+                <p>
+                    This is the collapsible content. It can be any element or React
+                    component you like.
+                </p>
+            </Collapsible>
+            <Collapsible trigger="Mais dois">
+                <p>
+                    This is the collapsible content. It can be any element or React
+                    component you like.
+                </p>
+            </Collapsible>
         </div>
     );
 }
+
+
 
 export default Ajuda;
